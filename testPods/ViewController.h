7815e1f1dd3e06b043477baf685702c708c070f2
zzh <zzh@zzhdemacbook-pro.local>
2015-12-09 15:40:52 +0800

    Initial Commit

---
+++ testPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testPods
+//
+//  Created by zzh on 15/12/9.
+//  Copyright © 2015年 zzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testPods/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testPods
+//
+//  Created by zzh on 15/12/9.
+//  Copyright © 2015年 zzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
